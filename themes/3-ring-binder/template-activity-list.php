cc9d5a670924ccf597d002eba9a84dc6c3d5b8f2
Dreas1 <dreas1@me.com>
2017-03-15 10:16:08 +0000

    Almost working activity page

--- themes/3-ring-binder/template-activity-list.php
+++ themes/3-ring-binder/template-activity-list.php
@@ -7,59 +7,58 @@ get_header();?>
 		
 		
 <?php '<!-- Start the header ------------------------------------------------------- -->' ?>
-			<H1><?php wp_title(' '); ?></H1>
-			<p>activity-list: build 0.1 Showing tags:
-			
-			<?php
-			$posttags = get_the_tags();
-			if ($posttags) {
-			  foreach($posttags as $tag) {
-				echo $tag->name . ' '; 
-			  }
-			}
-			
-			$firstPageTag =  $posttags[0]->name;
-			
-			echo '$firstPageTag '. $firstPageTag;
-			
-			?>
-			</p>
-<?php '<!-- End the header ---------------------------------------------------------- -->' ?>
+	<H1><?php wp_title(' '); ?></H1>
+	<p>activity-list: build 0.1:
+		<?php
+
+		$pageTagsList = get_the_tag_list('',',','');
+		$pageTagsArray = explode(',',$pageTagsList);
+		$pageTagsArrayLength = count($pageTagsArray);
+// 		echo 'page tags:'.$pageTagsList.'<br>'; 
+// 		echo 'page tags length:'.$pageTagsArrayLength.'<br>'; 
+// 		
+// 		for ($loop = 0; $loop < count($pageTagsArray); $loop++) {
+// 			echo 'pageTagsArray item:'.$pageTagsArray[$loop].'<br>'; 
+// 		} 
+// 		echo '<p>End of intro ---------------------</p>';	
+		?>
+	</p>
+<?php '<!-- Pages in Array ---------------------------------------------------------- -->' ?>
 				
-				
+	<?php
+		$args = array();
+		$mypages = get_pages($args);
+
+		foreach( $mypages as $page ) {	
+			$testId = $page->ID;
+			$tagList = get_the_tag_list('',',','',$testId);
+					
+			for ($loop = 0; $loop < count($pageTagsArray); $loop++) {
+			
+				$testTag = $pageTagsArray[$loop];
+				//echo 'pageTagsArray:'.$pageTagsArray[$loop].'<br>'; 
+				if( strpos($tagList,$testTag)){
+					echo 'match<br>';
+				}
+			} 	
+
+			echo 'title:'.$page->post_title.'<br>'; 
+			echo 'id:'.$testId.'<br>'; 
+			echo 'count:'.count($pageTagsArray).'<br>'; 
+			
+// 			echo 'tags:'.$tagList.'<br>'; 
+// 			echo 'tag count:'.strpos($tagList,$pageTagsList).'<br>';
+	   
+			echo '----------------<br>';
+// 	   
+// 	   
+		}
+		?>		
 
 <?php '<!-- Start the body ------------------------------------------------------- -->' ?>
-			<?php
-				$args = array();
-				
-				$mypages = get_pages($args);
-				
-				$arrlength = count($mypages);
-					
-			for($x = 0; $x < $arrlength; $x++) {
-				echo $mypages[$x];
-				echo "<br>";
-			}
 
-				foreach( $mypages as $page ) {		
-					$content = $page->post_content;
-					if ( ! $content ) // Check for empty page
-						continue;
-
-					$content = apply_filters( 'the_content', $content );
-
-				?>
-					<h2><a href="<?php echo get_page_link( $page->ID ); ?>">
-					<?php echo $page->post_title; ?> :
-					<?php echo $page->meta_key; ?>:
-					<?php echo $page->ID; ?></a></h2>
-					<div class="entry"><?php echo $content; ?></div>
-				<?php
-				}	
-				
-			?>
 <?php '<!-- 2 ------------------------------------------------------- -->' ?>	
-		
+<p>End of scripts ---------------------</p>		
 <?php '<!-- End the body ---------------------------------------------------------- -->' ?>
 	
 		</main><!-- .site-main -->
