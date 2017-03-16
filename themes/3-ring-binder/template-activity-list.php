0a518e9fb1929bdde8ca1e4d621d29f145f622a9
Dreas1 <dreas1@me.com>
2017-03-16 10:23:43 +0000

    Completed activity list

--- themes/3-ring-binder/template-activity-list.php
+++ themes/3-ring-binder/template-activity-list.php
@@ -13,16 +13,16 @@
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">	
 		
-<?php '<!-- Start the header ------------------------------------------------------- -->' ?>
+<?php echo'<!-- Start the header ------------------------------------------------------- -->' ?>
 
 <?php 
 	echo'<H1>'.get_post_field('post_title', $post->ID).'</H1>';
-	echo'<span class="site-info"> activity-list: build 0.1:</span>';
+	echo'<span class="site-info"> activity-list: build 0.2:</span>';
 	echo'<p>'.get_post_field('post_content', $post->ID).'</p>';
 ?>
 		
 	
-<?php '<!-- Pages in Array ---------------------------------------------------------- -->' ?>
+<?php echo'<!-- Pages in Array ---------------------------------------------------------- -->' ?>
 				
 	<?php
 		$args = array();
@@ -33,6 +33,7 @@
 			$tagList = get_the_tag_list('',' ','',$testId);
 			$displayFlag = true;
 			
+			//echo '----------------------------------------------------<br>';
 			// identify if item should be actively excluded ----------------------------
 			for ($loop = 0; $loop < count($excludeTags); $loop++) {
 				$testTag = $excludeTags[$loop];
@@ -41,7 +42,9 @@
 					$displayFlag = false;
 				}
 			}	
-				
+			//$displayFlag = true;	
+			//echo '$displayFlag:'.$displayFlag.'<br>';
+			
 			// 	insert content from other pages that match tags -------------------------
 			if ($displayFlag){	
 				for ($loop = 0; $loop < count($pageTagsArray); $loop++) {
@@ -50,6 +53,7 @@
 					//echo 'testing:'.$testTag.' is in:'.$tagList.'<br>';
 					//echo 'outcome:'.(strpos($tagList,$testTag)).'<br>';
 					//echo 'pageTagsArray:'.$pageTagsArray[$loop].'<br>'; 
+					
 					if( strpos($tagList,$testTag) || strpos($tagList,$testTag)===0){
 						//echo 'Yes<br><br>';
 						//echo '<h2>id:'.$testId.' title:'.$page->post_title. '</h2>';
@@ -66,11 +70,11 @@
 		}
 		?>		
 
-<?php '<!-- Start the body ------------------------------------------------------- -->' ?>
+<?php echo'<!-- Start the body ------------------------------------------------------- -->' ?>
 
-<?php '<!-- 2 ------------------------------------------------------- -->' ?>	
-<p>End of content ---------------------</p>		
-<?php '<!-- End the body ---------------------------------------------------------- -->' ?>
+<?php echo'<!-- 2 ------------------------------------------------------- -->' ?>	
+	
+<?php echo'<!-- End the body ---------------------------------------------------------- -->' ?>
 
 		</main><!-- .site-main -->
 	</div><!-- .content-area -->
