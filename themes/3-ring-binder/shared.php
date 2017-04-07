f69e7ef6b708e6ee97db9580bc3463e6cd81ed8f
Dreas1 <dreas1@me.com>
2017-04-07 10:56:00 +0100

    Updating shared Script

--- themes/3-ring-binder/shared.php
+++ themes/3-ring-binder/shared.php
@@ -16,7 +16,7 @@ function getContent($excludeTags){
 	$pageTagsArray = explode(',',$pageTagsList);
 	$pageTagsArrayLength = count($pageTagsArray);
 
-	$returnContent = '<span class="site-info debug">getContent:build 0.3</span><br>';
+	$returnContent = '<span class="site-info debug">getContent:build 0.5</span><br>';
 
 	$args = array(
 		'order'=>'ASC',
@@ -24,10 +24,35 @@ function getContent($excludeTags){
 	);
 	
 	$mypages = get_pages($args);
-	
-// 	foreach( $mypages as $page ) {	
-// 		$returnContent .= 'mo:'.$page->menu_order.' id: '.$page->ID.' title: '.$page->post_title.'<br>';
-// 	}
+
+	//---------------------------------------------------------------------------------
+	// Find if either 'Arrk' or 'Customer is in the tag list and add the opposite to the exclude list
+		
+	for ($loop = 0; $loop < $pageTagsArrayLength; $loop++) {
+		$singleTag = $pageTagsArray[$loop];
+		//$returnContent .='<span class="site-info debug">single tag: '.$singleTag.'</span><br>';
+		
+		If (findWord(false, $singleTag, 'Arrk')){
+			//$returnContent .= "Arrk Role<br>";
+			
+			array_push($excludeTags,"Customer");
+		}else{
+			//$returnContent .= "Not Arrk<br>";	
+		}
+		
+		If (findWord(false, $singleTag, 'Customer')){
+			//$returnContent .= "Got Customer<br>";
+			
+			array_push($excludeTags,"Arrk");
+		}else{
+			//$returnContent .= "Not Customer<br>";	
+		}
+		
+	}	
+
+	//$returnContent .='<span class="site-info debug">exclude tags: '.implode(" ",$excludeTags).'</span><br>';
+
+	//---------------------------------------------------------------------------------
 	
 	
 	foreach( $mypages as $page ) {	
@@ -35,21 +60,30 @@ function getContent($excludeTags){
 		$tagList = get_the_tag_list('',' ','',$testId);
 		$displayFlag = true;
 			
-		// identify if item should be actively excluded --------------------------		
-		// $returnContent .= 'mo:'.$page->menu_order.' id: '.$page->ID.' title: '.$page->post_title.'<br>';
+// 		$returnContent .= 'mo:'.$page->menu_order.' id: '.$page->ID.' title: '.$page->post_title.'<br>';
 // 		$returnContent .= 'excludeTags:'.implode($excludeTags).'<br>';
 // 		$returnContent .= 'tagList:'.$tagList.'<br>';
 	
+			//identify if item should be actively excluded --------------------------------		
+
+		// Exlude role pages from being shown within a role page -----------------------
 		for ($loop = 0; $loop < count($excludeTags); $loop++) {
 			$testTag = $excludeTags[$loop];
 			
 // 			$returnContent .= '$testTag:'.$testTag.'<br>';
 			
 			if( strpos($tagList,$testTag) || strpos($tagList,$testTag)===0){	
-				//$returnContent .= '<b>excluded</b><br>';
+// 				$returnContent .= '<b>excluded</b><br>';
 				$displayFlag = false;
 			}
 		}	
+		
+		
+
+		
+		
+		
+		
 		
 // 		$returnContent .= '<br>';
 // 		$returnContent .= '$displayFlag:'.$displayFlag.'<br>';
@@ -59,30 +93,23 @@ function getContent($excludeTags){
 			for ($loop = 0; $loop < count($pageTagsArray); $loop++) {
 				$testTag = $pageTagsArray[$loop];
 			
-// 				$returnContent .='testing:'.$testTag.' is in:'.$tagList.'<br>';
-// 				$returnContent .= 'outcome:'.(strpos($tagList,$testTag)).'<br>';
-// 				$returnContent .= 'pageTagsArray:'.$pageTagsArray[$loop].'<br>'; 
-							
+				// $returnContent .='testing:'.$testTag.' is in:'.$tagList.'<br>';
+// 				$returnContent .='outcome:'.(strpos($tagList,$testTag)).'<br>';
+				$returnContent .= $loop.': pageTagsArray:'.$pageTagsArray[$loop].' == '.$tagList.'<br>';
+
 				if( strpos($tagList,$testTag) || strpos($tagList,$testTag)===0){
 				
-					$returnContent .='<span class="site-info debug">testing:'.$testTag.' is in:'.$tagList.'</span><br>';
-					$returnContent .='<span class="site-info debug">outcome:'.(strpos($tagList,$testTag)).'</span><br>';
-					//$returnContent .= 'pageTagsArray:'.$pageTagsArray[$loop].'<br>'; 
-							
-					//$returnContent .= '<b>Match</b><br>';
-					//echo 'Yes<br><br>';
-					//$returnContent .= '<p>'.$page->menu_order.'<p>';	
-					//echo '<h2>id:'.$testId.' title:'.$page->post_title. '</h2>';
-					$returnContent .= '<span class="site-info debug">mo:'.$page->menu_order.' id: '.$page->ID.'</span><br>';	
+// 					$returnContent .='<span class="site-info debug">testing:'.$testTag.' is in:'.$tagList.'</span><br>';
+// 					$returnContent .='<span class="site-info debug">outcome:'.(strpos($tagList,$testTag)).'</span><br>';
+// 					$returnContent .= '<span class="site-info debug">mo:'.$page->menu_order.' id: '.$page->ID.'</span><br>';	
+					
+					
 					$returnContent .= '<h2>'.$page->post_title. '</h2>';		
-					//echo 'tags:'.$tagList.'<br>'; 
 					$returnContent .= '<span>'.$page->post_content. '</span>';		
-					//echo '<br>';
-					//break;
-				}else{
-					//echo 'Nope<br><br>';
+
 				}
 			}
+			$returnContent .= '<br>';
 		}
 	}
 		
@@ -91,7 +118,23 @@ function getContent($excludeTags){
 //
 //---------------------------------------------------------------------------------------
 //
+function findWord($caseSensitive, $testString, $findWord) {
 
+    if ($caseSensitive === true) {
+        if (strpos($testString, $findWord) !== false) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        if (strpos(strtolower($testString), strtolower($findWord)) !== false) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
 //
 //---------------------------------------------------------------------------------------
 //
